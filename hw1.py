a5da3fac501e76aee135bf12292fce4756d402fe
jamescastro98 <jamescastro9898@gmail.com>
2020-02-20 19:55:41 -0500

    I wrote tls instead of TLS I wanna die

--- hw1.py
+++ hw1.py
@@ -2,28 +2,39 @@ from scapy.all import *
 import sys
 import time
 from scapy.layers.http import HTTPRequest
+from scapy.layers.tls import *
+import cryptography
 '''
 Default to wlp3s0 
 '''
 def requests(pkt):
-    op="TLS"
-    src=pkts[i].getlayer(IP).src + ":" +str(pkts[i].getlayer(TCP).sport)
-    dst=pkts[i].getlayer(IP).dst+ ":" +str(pkts[i].getlayer(TCP).dport)
-    tim=str(time.asctime(time.localtime(x)))
-
+    op=""
     #Additional info for HTTP Requests, not TLS
     if pkt.haslayer(HTTPRequest):
+        src=pkt.getlayer(IP).src + ":" +str(pkt.getlayer(TCP).sport)
+        dst=pkt.getlayer(IP).dst+ ":" +str(pkt.getlayer(TCP).dport)
+        x=(pkt.getlayer(TCP).time)
+        tim=str(time.asctime(time.localtime(x)))
         op="HTTP"
         host = pkt[HTTPRequest].Host.decode()
         met=pkt[HTTPRequest].Method.decode()
         path = pkt[HTTPRequest].Path.decode()
-
-        #Capture Post Data - This can mean Login Credentials!!!
+        #Capture Post Data - This can mean Login Credentials!!!        
         if met=="POST":
             if pkt.haslayer(scapy.all.Raw):
                 cred=(str(pkt[scapy.all.Raw]))
                 cred=cred.replace('b','',1)
-                cred=cred,replace('\'','')
+                cred=cred.replace('\'','')
+        print(tim+ " " +op+" "+src +" -> " + dst +" " + host + " " +met + " "+ path)
+
+    elif pkt.haslayer('TLS'):
+        print("WE MADE IT")
+
+
+
+def tst(pkt):
+    pkt.show()
+    print("LALALALALLALA")
 
 face="wlp3s0"
 x=0
@@ -31,16 +42,7 @@ while(x<len(sys.argv)):
     if(sys.argv[x]=='-i'):
         face=sys.argv[x+1]
     x+=1
-
-pkts= sniff(filter='tcp',iface=face,prn=requests)
-'''
-for i in range(3):
-    #print(pkts[i].summary())
-    src=pkts[i].getlayer(IP).src + ":" +str(pkts[i].getlayer(TCP).sport)
-    dst=pkts[i].getlayer(IP).dst+ ":" +str(pkts[i].getlayer(TCP).dport)
-    tim=str(time.asctime(time.localtime(x)))
-    print(tim+ " "+ src + "->" + dst)
-    print("\n\n\n\n\n")
-    #print(pkts[i].getlayer(IP).dst)
-    x=(pkts[i].getlayer(TCP).time)
-'''
+load_layer("http")
+load_layer("tls")
+#filter='tcp'
+pkts= sniff(iface=face,prn=requests)
